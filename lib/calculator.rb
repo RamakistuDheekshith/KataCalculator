d04ce387b891049e591766acd86635ffe8f17b07
Dheekshith Ramakistu <dheekshith.r@gmail.com>
2025-04-28 17:32:00 +0530

    handle custom delimiters for input in format ***//[delimiter]\n[numbers…]***

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,13 @@
 class Calculator
   def add(input_string)
     return 0 if input_string.empty?
-    numbers = input_string.split(/[\n,]/).map(&:to_i)
+    if input_string.start_with?("//")
+      custom_delimiter = input_string.split("\n")[0][2]
+      numbers_input_string = input_string.split("\n")[1]
+      numbers = numbers_input_string.split(custom_delimiter).map(&:to_i)
+    else
+      numbers = input_string.split(/[\n,]/).map(&:to_i)
+    end
     numbers.sum
   end
 end
