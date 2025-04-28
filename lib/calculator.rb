23e609a026c1731422044c1fd59c91d33cf60026
Dheekshith Ramakistu <dheekshith.r@gmail.com>
2025-04-28 16:56:07 +0530

    Implement addition of multiple numbers

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,11 +1,7 @@
 class Calculator
   def add(input_string)
     return 0 if input_string.empty?
-    numbers = input_string.split(',')
-    if numbers.length == 1
-      return numbers[0].to_i
-    else
-      return numbers[0].to_i + numbers[1].to_i
-    end
+    numbers = input_string.split(',').map(&:to_i)
+    numbers.sum
   end
 end
