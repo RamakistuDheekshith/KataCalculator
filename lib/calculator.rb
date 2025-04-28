7baa70cb022d87389b24f82d445112e2ecc324a5
Dheekshith Ramakistu <dheekshith.r@gmail.com>
2025-04-28 23:19:27 +0530

    accept multiple custom delimiters in format '//[delimiter][delimiter]...\n[numbers]'

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -5,10 +5,14 @@ class Calculator
     if input_string.start_with?('//')
       # considering substring before \n from “//[delimiter]\n[numbers…]”
       delimiter_input = input_string.split("\n")[0]
-      # considering multi char delimiter when delimiter_input is '//[delimiter]'
-      # single char delimter when delimter_input is '//delimiter'
-      custom_delimiter = delimiter_input.scan(/\[(.*?)\]/)[0]? delimiter_input.scan(/\[(.*?)\]/)[0][0] : delimiter_input[2]
+      # considering single char delimter when delimter_input is '//delimiter'
+      # multi char delimiter when delimiter_input is '//[delimiter]'
       numbers_input_string = input_string.split("\n")[1]
+      if delimiter_input.scan(/\[(.*?)\]/).flatten.empty?
+        custom_delimiter =  delimiter_input[2]
+      else 
+        custom_delimiter = Regexp.new("[#{delimiter_input.scan(/\[(.*?)\]/).flatten.join('')}]")
+      end  
       numbers = numbers_input_string.split(custom_delimiter).map(&:to_i)
     else
       numbers = input_string.split(/[\n,]/).map(&:to_i)
