4291e55a368367a68074478260bb9eee54bf936b
Dheekshith Ramakistu <dheekshith.r@gmail.com>
2025-04-28 16:37:08 +0530

    Implement addition of one and two numbers

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,11 @@
 class Calculator
   def add(input_string)
     return 0 if input_string.empty?
+    numbers = input_string.split(',')
+    if numbers.length == 1
+      return numbers[0].to_i
+    else
+      return numbers[0].to_i + numbers[1].to_i
+    end
   end
 end
