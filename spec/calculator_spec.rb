ae6563c521f89e5882ab6418754567df98af9dec
Dheekshith Ramakistu <dheekshith.r@gmail.com>
2025-04-28 16:18:18 +0530

    add test for empty string

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,12 @@
 require 'spec_helper'
+require 'calculator'
+
+RSpec.describe Calculator do
+	let(:calculator) { Calculator.new }
+
+  context "add using input string" do
+		it "contains empty value in it" do
+			expect(calculator.add("")).to eq(0)
+		end
+	end
+end
